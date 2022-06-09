d82bd2a332562645cc976df6d44b0f6358806d12
Lira <lirabuet15@gmail.com>
2022-06-10 01:51:10 +0600

    added singly-or-doubly.py

---
+++ singly-or-doubly.py
@@ -0,0 +1,14 @@
+As = int(input('Reinforcement area(sq in)? '))
+b = int(input('width of beam(in)? '))
+d = float(input('What is reinforcement depth(in)? '))
+fc = int(input('What is the value of fc(psi)? '))
+fy = int(input('What is the value of fy(ksi)? '))
+b1 = 0.85 - ((0.05 * (fc - 4000)) / 1000)
+eu = 0.003
+ey = 0.004
+p1 = As / (b * d)
+p2 = 0.85 * b1 * (fc / fy) * (eu / (eu + ey))
+if p1 >= p2:
+    print('Double reinforcement')
+else:
+    print('Single reinforcement')
